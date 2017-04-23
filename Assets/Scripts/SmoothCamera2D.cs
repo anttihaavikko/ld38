ce4b43d08f5b14ef8fcc1fe4a750fca5d04dd297
Antti Haavikko <antti.haavikko@focus.fi>
2017-04-23 09:31:58 +0300

    Adjust bloom with bunny velocity

--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -8,11 +8,16 @@ public class SmoothCamera2D : MonoBehaviour {
 	public Transform target;
 
 	private VignetteAndChromaticAberration chroma;
+	private Bloom bloom;
 
 	private float shakeAmount = 0, shakeTime = 0;
 
+	private Rigidbody2D body;
+
 	void Start() {
 		chroma = GetComponent<VignetteAndChromaticAberration> ();
+		bloom = GetComponent<Bloom> ();
+		body = target.GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
@@ -30,6 +35,11 @@ public class SmoothCamera2D : MonoBehaviour {
 			float dist = Mathf.Clamp (target.transform.position.magnitude * 1.5f, 5f, 12f);
 
 			Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, dist, 0.025f);
+
+			float b = (body.velocity.magnitude > 5f) ? body.velocity.magnitude - 5f : 0f;
+			b = Mathf.Clamp(0.4f + b * 0.1f, 0.4f, 2f);
+			bloom.bloomIntensity = Mathf.Clamp(0.4f + b * 0.1f, 0.4f, 2f);
+			bloom.bloomThreshold = Mathf.Clamp(0.4f - b * 0.1f, 0.2f, 0.4f);
 		}
 
 	}
